4ee9db9edd859a146a6aa9454626bacd795eda16
Yurii Antonchyk <xsat.php@gmail.com>
2023-08-31 19:15:33 +0300

    Add simple gui

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from configparser import ConfigParser
 from platform import system
 from subprocess import check_output
 from re import search, Match
+from hashlib import md5
+from PySimpleGUI import theme, rgb, Window, Text, StatusBar, Button, WIN_CLOSED
 
 
 SETTINGS_FILENAME: str = 'settings.ini'
@@ -14,14 +16,60 @@ HOST_UNREACHABLE_PING_PATTERN = 'Destination host unreachable'
 LOST_PACKAGE_PING_PATTERN = 'Lost = 1'
 
 
+BLACK_COLOR = rgb(0, 0, 0)
+RED_COLOR = rgb(255, 0, 0)
+GREEN_COLOR = rgb(0, 255, 0)
+YELLOW_COLOR = rgb(255, 255, 0)
+
+
 def main() -> None:
+    def generate_key(left_name: str, right_name: str) -> str:
+        return md5(f'-{left_name}-{right_name}-'.encode()).hexdigest()
+
+    theme('Dark')
+
+    layout = []
+
     config: ConfigParser = ConfigParser()
     config.read(SETTINGS_FILENAME)
 
     for section_name in config.sections():
+        layout.append([Text(section_name)])
+
         for value_name in config[section_name]:
-            address = config[section_name][value_name]
-            print(value_name, config[section_name][value_name], ping(address))
+            layout.append([StatusBar(value_name, key=generate_key(section_name, value_name), enable_events=True)])
+
+            # address = config[section_name][value_name]
+            # print(value_name, config[section_name][value_name], ping(address))
+
+    layout.append([
+        Button('Test', button_color=YELLOW_COLOR),
+        Button('Cancel', button_color=RED_COLOR)])
+
+    window = Window('Window Title', layout)
+
+    while True:
+        event, values = window.read()
+        if event == WIN_CLOSED or event == 'Cancel':
+            break
+        if event == 'Test':
+            for section_name in config.sections():
+                for value_name in config[section_name]:
+                    address = config[section_name][value_name]
+                    if ping(address):
+                        window[generate_key(section_name, value_name)].update(background_color=GREEN_COLOR)
+                    else:
+                        window[generate_key(section_name, value_name)].update(background_color=RED_COLOR)
+
+    window.close()
+
+    # config: ConfigParser = ConfigParser()
+    # config.read(SETTINGS_FILENAME)
+    #
+    # for section_name in config.sections():
+    #     for value_name in config[section_name]:
+    #         address = config[section_name][value_name]
+    #         print(value_name, config[section_name][value_name], ping(address))
 
 
 def ping(hostname: str) -> bool:
