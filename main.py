a17f7c10e474aa32cd4e375f80b12b5bf6882a7f
Yurii Antonchyk <xsat.php@gmail.com>
2023-08-31 23:10:47 +0300

    Added last test time

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser
 from hashlib import md5
 from PySimpleGUI import theme, rgb, theme_element_background_color, Window, Text, StatusBar, Button, Element, WIN_CLOSED
 from ping import ping
-
+from time import strftime
 
 DEFAULT_THEME: str = 'Dark'
 
@@ -11,10 +11,11 @@ SETTINGS_FILENAME: str = 'settings.ini'
 BLACK_COLOR: str = rgb(0, 0, 0)
 RED_COLOR: str = rgb(255, 0, 0)
 GREEN_COLOR: str = rgb(0, 255, 0)
-YELLOW_COLOR: str = rgb(255, 255, 0)
+BLUE_COLOR: str = rgb(0, 255, 255)
 
 TEST_BUTTON_KEY: str = 'dd8fe8f21b59e4d70f144900fbf286a2'
 CLOSE_BUTTON_KEY: str = 'e48f38b2f4d895fe81e9604877fee9d9'
+LAST_CHECK_TEXT_KEY: str = 'ce2082b1ba08ac910de71e10a8643a78'
 
 
 def main() -> None:
@@ -26,6 +27,11 @@ def main() -> None:
             return GREEN_COLOR
 
         return RED_COLOR
+
+    def get_last_check_text() -> str:
+        return strftime('%Y-%m-%d %H:%M:%S')
+
+        # return '0000-00-00 00:00:01'
 
     theme(DEFAULT_THEME)
 
@@ -44,8 +50,9 @@ def main() -> None:
             layout.append([StatusBar(value_name, key=element_key)])
 
     layout.append([
-        Button('Test', key=TEST_BUTTON_KEY, button_color=YELLOW_COLOR),
-        Button('Close', key=CLOSE_BUTTON_KEY, button_color=RED_COLOR)])
+        Button('Test', key=TEST_BUTTON_KEY, button_color=BLUE_COLOR),
+        Button('Close', key=CLOSE_BUTTON_KEY, button_color=RED_COLOR),
+        Text('0000-00-00 00:00:00', key=LAST_CHECK_TEXT_KEY)])
 
     window: Window = Window('Pinger', layout, resizable=True)
     default_element_background_color: str = theme_element_background_color()
@@ -60,6 +67,8 @@ def main() -> None:
             for element_key1 in element_keys:
                 window[element_key1].update(background_color=default_element_background_color)
 
+            last_check_text: str = get_last_check_text()
+            window[LAST_CHECK_TEXT_KEY].update(value=last_check_text)
             window.refresh()
 
             for section_name in config.sections():
