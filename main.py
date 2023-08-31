2835e3bbb9b98304f826ed79d4eb027959499aea
Yurii Antonchyk <xsat.php@gmail.com>
2023-08-31 16:57:04 +0300

    Improve ping func

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from configparser import ConfigParser
 from platform import system
-from subprocess import call, DEVNULL, STDOUT
+from subprocess import check_output
+from re import search, Match
+
 
 SETTINGS_FILENAME: str = 'settings.ini'
 
 WINDOWS_OS: str = 'Windows'
 WINDOWS_PING_PARAM: str = '-n'
 OTHER_PING_PARAM: str = '-c'
+
+HOST_UNREACHABLE_PING_PATTERN = 'Destination host unreachable'
+LOST_PACKAGE_PING_PATTERN = 'Lost = 1'
 
 
 def main() -> None:
@@ -20,15 +25,21 @@ def main() -> None:
 
 
 def ping(hostname: str) -> bool:
+    def is_matched(pattern: str, string: str) -> bool:
+        search_result: Match[str] = search(pattern, string)
+
+        return search_result is not None
+
     if system() == WINDOWS_OS:
         param = WINDOWS_PING_PARAM
     else:
         param = OTHER_PING_PARAM
 
     command: list[str] = ['ping', param, '1', hostname]
+    command_result: str = str(check_output(command))
 
-    # todo: Process timeout cases
-    return call(command, stdout=DEVNULL, stderr=STDOUT) == 0
+    return (not is_matched(HOST_UNREACHABLE_PING_PATTERN, command_result)
+            and not is_matched(LOST_PACKAGE_PING_PATTERN, command_result))
 
 
 if __name__ == '__main__':
